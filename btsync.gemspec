4373d23c8cc351e05e2462c500a290b865e5d203
Chris MacNaughton <chmacnaughton@gmail.com>
2013-06-04 14:30:54 -0400

    several new methods work

--- btsync.gemspec
+++ btsync.gemspec
@@ -23,4 +23,5 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency "httparty"
   spec.add_runtime_dependency "nokogiri"
+  spec.add_runtime_dependency "json"
 end
--- lib/btsync.rb
+++ lib/btsync.rb
@@ -1,12 +1,80 @@
 require 'httparty'
 require 'nokogiri'
+require 'json'
 
 class Btsync
   include HTTParty
-
   def initialize uri=nil, port=nil
+    @cache = {
+      :folder => 0,
+      :secret => 0,
+      :settings => 0,
+      :os_type => 0,
+      :new_version => 0
+    }
     @uri = uri
     @port = port
+    secret
+  end
+  def get_folders
+    get_folder_list["folders"]
+  end
+  def get_speed
+    s = get_folder_list["speed"].split(", ")
+    up = s[0].split(" ")
+    down = s[1].split(" ")
+    {:up => up[0], :down => down[0], :metric => up[1]}
+  end
+
+  def get_settings
+    time = DateTime.now.strftime("%s").to_i
+    if time > @cache[:settings] + 600
+      res = self.class.get(path('getsettings'), :query => {:output => "json"}, :headers => {"Cookie" => cookies })
+      @settings = res.parsed_response["settings"]
+      @cache[:settings] = time
+    end
+    @settings
+  end
+  def get_os_type
+    time = DateTime.now.strftime("%s").to_i
+    if time > @cache[:os_type] + 600
+      res = self.class.get(path('getostype'), :query => {:output => "json"}, :headers => {"Cookie" => cookies })
+      @os_type = res.parsed_response["os"]
+      @cache[:os_type] = time
+    end
+    @os_type
+  end
+  def get_version
+    time = DateTime.now.strftime("%s").to_i
+    if time > @cache[:version] + 600
+      res = self.class.get(path('getversion'), :query => {:output => "json"}, :headers => {"Cookie" => cookies })
+      @version = res.parsed_response
+      @cache[:version] = time
+    end
+    @version
+  end
+  def check_new_version
+    time = DateTime.now.strftime("%s").to_i
+    if time > @cache[:new_version] + 600
+      res = self.class.get(path('checknewversion'), :query => {:output => "json"}, :headers => {"Cookie" => cookies })
+      @new_version = res.parsed_response["version"]
+      @cache[:new_version] = time
+    end
+    @new_version
+  end
+  def get_dir with_dir
+    res = self.class.get(path('getdir'), :query => {:dir => with_dir, :output => "json"}, :headers => {"Cookie" => cookies })
+    res.parsed_response["folders"]
+  end
+  private
+  def get_folder_list
+    time = DateTime.now.strftime("%s").to_i
+    if time > @cache[:folder] + 600
+      res = self.class.get(path('getsyncfolders'), :query => {:output => "json"}, :headers => {"Cookie" => cookies })
+      @folder_list = res.parsed_response
+      @cache[:folder] = time
+    end
+    @folder_list
   end
 
   def port
@@ -16,12 +84,20 @@ class Btsync
     @uri ||= "http://localhost"
   end
   def secret
-    @secret ||= Nokogiri::HTML(self.class.get(path + 'token.html?t=' + DateTime.now.strftime('%s'))).search('#token').text
+    time = DateTime.now.strftime("%s").to_i
+    if time > @cache[:secret] + 600
+      @secret = Nokogiri::HTML(request_token).search('#token').text
+      @cache[:secret] = time
+    end
+    @secret
   end
-  def path
-    "#{uri}:#{port}/gui/"
+  def cookies
+    @cookies ||= request_token.headers["set-cookie"].split("; ")[0]
   end
-  def action action_name
-    self.class.get(path, :query => {:token => secret, :action => action_name})
+  def request_token
+    @request_token ||= self.class.get("#{uri}:#{port}/gui/token.html?t=" + DateTime.now.strftime('%s'))
+  end
+  def path action_name
+    "#{uri}:#{port}/gui/?token=#{secret}&action=#{action_name}"
   end
 end
