415e8ce04363b53b9083990920328be7e053bd8d
Chris MacNaughton <chmacnaughton@gmail.com>
2013-06-21 12:54:24 -0400

    adding correct headers?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ filename = File.expand_path(filename)
 Dir[filename].each { |f| require f }
 
 VCR.configure do |c|
-  c.default_cassette_options = { record: :all }
+  c.default_cassette_options = { record: :all ) }
   c.cassette_library_dir = 'spec/fixtures/cassettes'
   c.hook_into :webmock # or :fakeweb
   c.ignore_request { |request| URI(request.uri).host =~ /127\.0\.0\.1/ }
